0b3379ee2ee16c460b0a2872c49cf650e6d0b7cf
wcyat <wcyat@wcyat.me>
2021-10-31 02:22:24 +0800

    update

--- halloween/halloween.js
+++ halloween/halloween.js
@@ -2,12 +2,12 @@ function sleep (ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-function count () {
+async function count () {
   if (document.getElementById('time').innerHTML == 0) {
     window.location.href = 'https://js.johnniewong.tech/dev/4/'
   }
   document.getElementById('time').innerHTML -= 1;
-  sleep(1000)
+  await sleep(1000)
   count()
 }
 
