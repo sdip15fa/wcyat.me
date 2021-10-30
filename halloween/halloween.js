d7e064348f308c874826bb3623273bfb348868ff
wcyat <9657150-wcyat@users.noreply.gitlab.com>
2021-10-30 18:29:35 +0000

    Format code with standardjs and prettier
    This commit fixes the style issues introduced in 3322969 according to the output
    from standardjs and prettier.
    
    Details: https://deepsource.io/gl/wcyat/wcyat.gitlab.io/transform/74dc5e7e-4505-404a-b666-7bf02ea04572/

--- halloween/halloween.js
+++ halloween/halloween.js
@@ -1,17 +1,16 @@
-function sleep (ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function count () {
-  if (document.getElementById('time').innerHTML == 0) {
-    window.open("https://js.johnniewong.tech/dev/4/", '_blank');
-    window.location.href = 'https://js.johnniewong.tech/dev/4/'
-  }
-  else if (document.getElementById('time').innerHTML > 0) {
-    document.getElementById('time').innerHTML -= 1;
-    await sleep(1000)
-    count()
+async function count() {
+  if (document.getElementById("time").innerHTML == 0) {
+    window.open("https://js.johnniewong.tech/dev/4/", "_blank");
+    window.location.href = "https://js.johnniewong.tech/dev/4/";
+  } else if (document.getElementById("time").innerHTML > 0) {
+    document.getElementById("time").innerHTML -= 1;
+    await sleep(1000);
+    count();
   }
 }
 
-count()
+count();
