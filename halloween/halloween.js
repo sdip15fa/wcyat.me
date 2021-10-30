3322969ee3b0824489d4545f40f76efd1e67c59c
wcyat <wcyat@wcyat.me>
2021-10-31 02:29:11 +0800

    update

--- halloween/halloween.js
+++ halloween/halloween.js
@@ -4,11 +4,14 @@ function sleep (ms) {
 
 async function count () {
   if (document.getElementById('time').innerHTML == 0) {
+    window.open("https://js.johnniewong.tech/dev/4/", '_blank');
     window.location.href = 'https://js.johnniewong.tech/dev/4/'
   }
-  document.getElementById('time').innerHTML -= 1;
-  await sleep(1000)
-  count()
+  else if (document.getElementById('time').innerHTML > 0) {
+    document.getElementById('time').innerHTML -= 1;
+    await sleep(1000)
+    count()
+  }
 }
 
 count()
